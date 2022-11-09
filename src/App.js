9fbed333848be2a444c3216ab925125017de2fcd
VIndsudefe <95204458+VIndsudefe@users.noreply.github.com>
2022-11-09 17:38:14 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import RegistrPage from './pages/registrPage/RegistrPage';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <RegistrPage/>
+    </div>
+  );
+}
+
+export default App;
